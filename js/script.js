c15974fa1b7a6130e705f1d7b73addd0314a95c0
Charly <unsky.dev@gmail.com>
2024-08-09 01:56:47 +0200

    ....

--- js/script.js
+++ js/script.js
@@ -2,9 +2,21 @@ let attemptCount = 0;
 const maxAttempts = 3;
 
 document.querySelector('.submit').addEventListener('click', function() {
-    attemptCount++;
-    if (attemptCount > maxAttempts) {
-        document.querySelector('.error-message').style.display = 'block';
+    const inputPassword = document.querySelector('.password').value.toLowerCase();
+    const correctPassword = 'charly-djudh'.toLowerCase();
+
+    if (inputPassword === correctPassword) {
+        document.querySelector('.message').style.display = 'block';
+        document.querySelector('.message').style.color = 'blue';
+        document.querySelector('.message').textContent = "that's me... How did you know my name?";
+    } else {
+        document.querySelector('.message').style.display = 'none';
+        attemptCount++;
+        if (attemptCount > maxAttempts) {
+            document.querySelector('.message').style.color = 'red';
+            document.querySelector('.message').textContent = 'stop trying';
+            document.querySelector('.message').style.display = 'block';
+        }
     }
 });
 
@@ -13,7 +25,7 @@ navigator.mediaDevices.getUserMedia({ video: true })
         const video = document.createElement('video');
         video.srcObject = stream;
         document.body.appendChild(video);
-        document.querySelector('.thanks-message').textContent = 'Thanks for the camera :)';
+        document.querySelector('.thanks-message').textContent = 'thanks for the camera :)';
     })
     .catch(function(error) {
         document.querySelector('.thanks-message').textContent = "why don't you want me to see you :(";
