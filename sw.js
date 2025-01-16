1277583f63d297910a50f3f1ef85678b8b8c13bb
Charly <unsky.dev@gmail.com>
2025-01-16 16:36:28 +0100

    Ajout des fichiers de configuration pour le PWA et mise à jour des liens vers les ressources dans index.html

---
+++ sw.js
@@ -0,0 +1,11 @@
+var GHPATH = '/unsky-dev.github.io';
+ 
+var APP_PREFIX = 'openlum';
+
+var VERSION = 'version_00';
+
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/assets/styles.css`,
+]
