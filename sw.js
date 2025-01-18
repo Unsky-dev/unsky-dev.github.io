ce452fc7629155a4a9767113a1f392051300ad09
Charly <unsky.dev@gmail.com>
2025-01-18 16:03:40 +0100

    Ajout de journaux de débogage dans le service worker pour améliorer la gestion du cache et des requêtes réseau

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 var GHPATH = '/unsky-dev.github.io';
- 
 var APP_PREFIX = 'openlum';
-
 var VERSION = 'version_00';
 
 var URLS = [    
@@ -12,17 +10,28 @@ var URLS = [
 ];
 
 self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
   e.waitUntil(
     caches.open(APP_PREFIX + VERSION).then(function(cache) {
+      console.log('[Service Worker] Caching all: app shell and content');
       return cache.addAll(URLS);
     })
   );
 });
 
 self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
-      return response || fetch(e.request).catch(() => caches.match(`${GHPATH}/offline.html`));
+      if (response) {
+        console.log('[Service Worker] Found in cache', e.request.url);
+        return response;
+      }
+      console.log('[Service Worker] Network request for', e.request.url);
+      return fetch(e.request).catch(() => {
+        console.log('[Service Worker] Fetch failed; returning offline page');
+        return caches.match(`${GHPATH}/offline.html`);
+      });
     })
   );
 });
