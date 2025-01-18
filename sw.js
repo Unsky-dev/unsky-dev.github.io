1e571fe52a619e633a1916d1de7ded488872d500
Charly <unsky.dev@gmail.com>
2025-01-18 17:03:28 +0100

    Ajout de la gestion de l'activation du service worker et nettoyage des caches obsolètes

--- sw.js
+++ sw.js
@@ -2,11 +2,10 @@ var GHPATH = '/unsky-dev.github.io';
 var APP_PREFIX = 'openlum';
 var VERSION = 'version_02';
 
-var URLS = [    
-  `${GHPATH}/`,
+var URLS = [
   `${GHPATH}/index.html`,
   `${GHPATH}/assets/css/styles.css`,
-  `${GHPATH}/offline.html`
+  `${GHPATH}/offline.html`,
 ];
 
 self.addEventListener('install', function(e) {
@@ -19,19 +18,33 @@ self.addEventListener('install', function(e) {
   );
 });
 
+self.addEventListener('activate', function(e) {
+  console.log('[Service Worker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== APP_PREFIX + VERSION) {
+            console.log('[Service Worker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       if (response) {
         console.log('[Service Worker] Found in cache', e.request.url);
-        return response; // Utilisez la réponse en cache directement.
+        return response;
       }
       console.log('[Service Worker] Network request for', e.request.url);
       return fetch(e.request).then(function(networkResponse) {
         if (networkResponse.redirected) {
-          // Cloner et nettoyer les réponses redirigées.
-          console.log('[Service Worker] Redirected response from network', e.request.url);
           const cleanResponse = networkResponse.clone();
           return cleanResponse;
         }
@@ -39,7 +52,7 @@ self.addEventListener('fetch', function(e) {
       }).catch(() => {
         console.log('[Service Worker] Fetch failed; returning offline page');
         if (e.request.mode === 'navigate') {
-          return caches.match(`./offline.html`);
+          return caches.match(`${GHPATH}/offline.html`);
         }
       });
     })
