673742b8b0da57cfaa153bbe637672e8cdd0b10c
Charly <unsky.dev@gmail.com>
2025-01-18 17:09:24 +0100

    Amélioration des messages de journalisation et nettoyage du code dans le service worker

--- sw.js
+++ sw.js
@@ -8,24 +8,24 @@ var URLS = [
   `${GHPATH}/offline.html`,
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.open(APP_PREFIX + VERSION).then(function(cache) {
-      console.log('[Service Worker] Caching all: app shell and content');
+    caches.open(APP_PREFIX + VERSION).then(function (cache) {
+      console.log('[Service Worker] Caching app shell and content');
       return cache.addAll(URLS);
     })
   );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
   console.log('[Service Worker] Activate');
   e.waitUntil(
-    caches.keys().then(function(keyList) {
+    caches.keys().then(function (keyList) {
       return Promise.all(
-        keyList.map(function(key) {
+        keyList.map(function (key) {
           if (key !== APP_PREFIX + VERSION) {
-            console.log('[Service Worker] Removing old cache', key);
+            console.log('[Service Worker] Removing old cache:', key);
             return caches.delete(key);
           }
         })
@@ -34,27 +34,29 @@ self.addEventListener('activate', function(e) {
   );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   console.log('[Service Worker] Fetch', e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       if (response) {
-        console.log('[Service Worker] Found in cache', e.request.url);
-        return response;
+        console.log('[Service Worker] Found in cache:', e.request.url);
+        return response; // Retourne la réponse mise en cache
       }
-      console.log('[Service Worker] Network request for', e.request.url);
-      return fetch(e.request).then(function(networkResponse) {
-        if (networkResponse.redirected) {
-          const cleanResponse = networkResponse.clone();
-          return cleanResponse;
-        }
-        return networkResponse;
-      }).catch(() => {
-        console.log('[Service Worker] Fetch failed; returning offline page');
-        if (e.request.mode === 'navigate') {
-          return caches.match(`${GHPATH}/offline.html`);
-        }
-      });
+      console.log('[Service Worker] Network request for:', e.request.url);
+      return fetch(e.request)
+        .then(function (networkResponse) {
+          if (networkResponse.redirected) {
+            console.log('[Service Worker] Handling redirected response');
+            return networkResponse.clone(); 
+          }
+          return networkResponse;
+        })
+        .catch(() => {
+          console.log('[Service Worker] Fetch failed; returning offline page');
+          if (e.request.mode === 'navigate') {
+            return caches.match(`${GHPATH}/offline.html`);
+          }
+        });
     })
   );
 });
