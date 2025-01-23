a57e90341b79105184d437765bd3c83a16360580
Charly <unsky.dev@gmail.com>
2025-01-23 16:34:49 +0100

    Ajout de la couleur du texte d'information lors de l'initialisation du tag NFC

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,6 +136,7 @@ nfc.querySelector('.scanButton').addEventListener('click', async () => {
     info.style.display = 'block';
     document.querySelector('.writeButton').style.display = 'none';
     document.querySelector('.scanButton').style.display = 'none';
+    info.style.color = 'black';
     info.textContent = 'Veuillez approcher le tag NFC...';
     try {
         const ndef = new NDEFReader();
